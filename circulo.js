dfb724fb12333fae8d14c406288a08bf0c942dd4
unknown <FISC@.(none)>
2011-10-01 12:24:45 -0500

    Subiendo mi codito

---
+++ circulo.js
@@ -0,0 +1,25 @@
+function Circulo(juego, x, y ) {
+	this.juego = juego;
+	this.x = x;
+	this.y = y;
+	this.remover = false;
+}
+
+Circulo.prototype.actualizar = function() { 
+    if (this.x < this.juego.ctx.canvas.width) {
+        this.x += 2;
+    }
+    else {
+        this.x = 0;
+    }
+};
+
+Circulo.prototype.dibujar = function(ctx) { 
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.fillStyle = "black";
+    ctx.arc(this.x, this.y, 50, 0, Math.PI*2, false);
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+};
